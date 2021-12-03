4e72f8ca24e68c61c13ab85844f50c00187035b8
Agif1234 <muhalgifari02@gmail.com>
2021-12-03 14:56:32 +0700

    upload pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Muhammad Algifari
+ Saya tinggal di daerah Teluknaga
+ hobi saya adalah Riding";
+ }
+}
